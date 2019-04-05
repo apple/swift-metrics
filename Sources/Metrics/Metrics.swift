6bfb4d316b7e0a79a4a16ec4b20e1bd3d903d89c
tomer doron <tomerd@apple.com>
2019-04-05 13:40:48 -0700

    prepare to release
    motivation: the sswg voted to adopt the API. this is to prepare to a release
    
    changes:
    * rewrite readme
    * add API docs
    * add utilitiy scripts and docker setup for CI
    * adjust linux tests

--- Sources/Metrics/Metrics.swift
+++ Sources/Metrics/Metrics.swift
@@ -1,9 +1,28 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Metrics API open source project
+//
+// Copyright (c) 2018-2019 Apple Inc. and the Swift Metrics API project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift Metrics API project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 @_exported import CoreMetrics
 @_exported import class CoreMetrics.Timer
 @_exported import Foundation
 
-// Convenience for measuring duration of a closure
 public extension Timer {
+    /// Convenience for measuring duration of a closure
+    ///
+    /// - parameters:
+    ///     - label: The label for the Timer.
+    ///     - dimensions: The dimensions for the Timer.
+    ///     - body: Closure to run & record.
     @inlinable
     static func measure<T>(label: String, dimensions: [(String, String)] = [], body: @escaping () throws -> T) rethrows -> T {
         let timer = Timer(label: label, dimensions: dimensions)
@@ -15,13 +34,19 @@ public extension Timer {
     }
 }
 
-// Convenience for using Foundation and Dispatch
 public extension Timer {
+    /// Convenience for recording a duration based on TimeInterval
+    ///
+    /// - parameters:
+    ///     - duration: The duration to record.
     @inlinable
     func record(_ duration: TimeInterval) {
         self.recordSeconds(duration)
     }
 
+    /// Convenience for recording a duration based on DispatchTimeInterval
+    /// - parameters:
+    ///     - duration: The duration to record.
     @inlinable
     func record(_ duration: DispatchTimeInterval) {
         switch duration {
--- Tests/MetricsTests/MetricsTests.swift
+++ Tests/MetricsTests/MetricsTests.swift
@@ -56,25 +56,43 @@ class MetricsExtensionsTests: XCTestCase {
         let nano = DispatchTimeInterval.nanoseconds(Int.random(in: 1 ... 500))
         timer.record(nano)
         XCTAssertEqual(testTimer.values.count, 1, "expected number of entries to match")
-        XCTAssertEqual(.nanoseconds(Int(testTimer.values[0].1)), nano, "expected value to match")
+        XCTAssertEqual(Int(testTimer.values[0].1), nano.nano(), "expected value to match")
         // micro
         let micro = DispatchTimeInterval.microseconds(Int.random(in: 1 ... 500))
         timer.record(micro)
         XCTAssertEqual(testTimer.values.count, 2, "expected number of entries to match")
-        XCTAssertEqual(.nanoseconds(Int(testTimer.values[1].1)), micro, "expected value to match")
+        XCTAssertEqual(Int(testTimer.values[1].1), micro.nano(), "expected value to match")
         // milli
         let milli = DispatchTimeInterval.milliseconds(Int.random(in: 1 ... 500))
         timer.record(milli)
         XCTAssertEqual(testTimer.values.count, 3, "expected number of entries to match")
-        XCTAssertEqual(.nanoseconds(Int(testTimer.values[2].1)), milli, "expected value to match")
+        XCTAssertEqual(Int(testTimer.values[2].1), milli.nano(), "expected value to match")
         // seconds
         let sec = DispatchTimeInterval.seconds(Int.random(in: 1 ... 500))
         timer.record(sec)
         XCTAssertEqual(testTimer.values.count, 4, "expected number of entries to match")
-        XCTAssertEqual(.nanoseconds(Int(testTimer.values[3].1)), sec, "expected value to match")
+        XCTAssertEqual(Int(testTimer.values[3].1), sec.nano(), "expected value to match")
         // never
         timer.record(DispatchTimeInterval.never)
         XCTAssertEqual(testTimer.values.count, 5, "expected number of entries to match")
         XCTAssertEqual(testTimer.values[4].1, 0, "expected value to match")
+    }
+}
+
+// https://bugs.swift.org/browse/SR-6310
+extension DispatchTimeInterval {
+    func nano() -> Int {
+        switch self {
+        case .nanoseconds(let value):
+            return value
+        case .microseconds(let value):
+            return value * 1_000
+        case .milliseconds(let value):
+            return value * 1_000_000
+        case .seconds(let value):
+            return value * 1_000_000_000
+        case .never:
+            return 0
+        }
     }
 }
