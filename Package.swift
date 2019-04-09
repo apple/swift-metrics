0b2fb92ce969b1de438535a3d13c735262e3a8db
tomer doron <tomerd@apple.com>
2019-04-09 14:43:38 -0700

    update package name

--- Package.swift
+++ Package.swift
@@ -1,9 +1,22 @@
 // swift-tools-version:4.2
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Metrics API open source project
+//
+// Copyright (c) 2018-2019 Apple Inc. and the Swift Metrics API project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift Metrics API project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 
 import PackageDescription
 
 let package = Package(
-    name: "metrics",
+    name: "swift-metrics",
     products: [
         .library(name: "CoreMetrics", targets: ["CoreMetrics"]),
         .library(name: "Metrics", targets: ["Metrics"]),
