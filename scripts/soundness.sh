dffbbf2445156208fe1e0700ad08f9a8fedcccac
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2022-01-19 08:13:36 +0900

    support the year 2021

--- scripts/soundness.sh
+++ scripts/soundness.sh
@@ -32,7 +32,7 @@ here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function replace_acceptable_years() {
     # this needs to replace all acceptable forms with 'YEARS'
-    sed -e 's/2018-2019/YEARS/' -e 's/2019/YEARS/'
+    sed -e 's/2018-2019/YEARS/' -e 's/2019/YEARS/' -E 's/2021/YEARS/'
 }
 
 printf "=> Checking linux tests... "
