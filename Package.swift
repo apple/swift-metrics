f105769daafeb151c42ef210449c9a0ad2a7a8f9
Rick Newton-Rogers <rnro@apple.com>
2024-12-10 10:44:00 +0000

    Enable MemberImportVisibility check on all targets

--- Package.swift
+++ Package.swift
@@ -46,3 +46,11 @@ for target in package.targets {
     settings.append(.enableExperimentalFeature("StrictConcurrency=complete"))
     target.swiftSettings = settings
 }
+
+for target in package.targets {
+    if target.type != .plugin {
+        var settings = target.swiftSettings ?? []
+        settings.append(.enableUpcomingFeature("MemberImportVisibility"))
+        target.swiftSettings = settings
+    }
+}
